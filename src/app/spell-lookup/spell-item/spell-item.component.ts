a714338fb99c2fcdb5ff8208314ad4e340ec194b
Joshua Sexton <joshua.sexton@ad.diva-e.com>
2019-11-27 13:31:52 +0100

    feature: spell-lookup module, routing, and file structure for spell lookup

---
+++ src/app/spell-lookup/spell-item/spell-item.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-spell-item',
+  templateUrl: './spell-item.component.html',
+  styleUrls: ['./spell-item.component.scss']
+})
+export class SpellItemComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ src/app/spell-lookup/spell-list/spell-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-spell-list',
+  templateUrl: './spell-list.component.html',
+  styleUrls: ['./spell-list.component.scss']
+})
+export class SpellListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
